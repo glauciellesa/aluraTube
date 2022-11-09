d97dbfa3a22bbdac425172873b09e30f7df14ac7
Glaucielle <glauciellesa@gmail.com>
2022-11-09 10:36:08 +0100

    First commit

---
+++ pages/components/Header.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import config from "../../config.json"
+
+const StyledHeader = styled.div`
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+}
+.user-info {
+    margin-top: 60px;
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 36px 32px;
+    gap: 16px;
+  }
+`;
+
+function Header() {
+  return (
+    <StyledHeader>
+      <section className="user-info">
+        <img src={`https://github.com/${config.github}.png`} />
+        <div>
+          <h2>{config.name} </h2>
+          <p>{config.job}</p>
+        </div>
+      </section>
+    </StyledHeader>
+  );
+}
+
+export default Header
---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import config from "../config.json";
+import { CSSReset } from "./components/CSSReset";
+import Menu from "./components/Menu"
+import Header from "./components/Header";
+import Timeline from "./components/Timeline";
+
+function HomePage() {
+  return (
+    <>
+    <CSSReset />
+      <div>
+        <Menu />
+        <Header />
+        <Timeline playlists={config.playlists} />
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
+
+
+
