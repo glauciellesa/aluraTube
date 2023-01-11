673dc53f752f0681bc00a8cd0817f9e7477719e8
Glaucielle Sá <65506911+glauciellesa@users.noreply.github.com>
2023-01-11 09:45:56 +0100

    Resize toggle buttons

--- src/components/Menu/ToggleTheme.jsx
+++ src/components/Menu/ToggleTheme.jsx
@@ -5,31 +5,33 @@ import styled from "styled-components"
 const StyledToggle = styled.div`
   background-color: #333333;
   border: 0;
-  padding: 3px;
-  font-size: 12px;
-  width: 50px;
-  height: 25px;
+  padding: 2px;
+  font-size: 10px;
+  width: 32px;
+  height: 16px;
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   border-radius: 10000px;
   position: relative;
+
   label {
-    width: 40px;
+    width: 30px;
   }
   span {
     display: inline-flex;
-    width: 20px;
-    height: 20px;
+    width: 12px;
+    height: 12px;
     align-items: center;
-    justify-content: center;
+    justify-content: space-between;
+    padding-left: 2px;
   }
 
   label:before {
     content: "";
     background-color: #fafafa;
     border: 1px solid #333333;
-    width: 24px;
-    height: 24px;
+    width: 14px;
+    height: 14px;
     border-radius: 100%;
     position: absolute;
     top: 0;
